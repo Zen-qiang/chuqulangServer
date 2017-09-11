ebf1b7b2cdaa2f06c4e7a3aceb46413f406cb7ee
xu.xiaohuan <xu.xiaohuan@dingliantech.com>
2017-09-11 11:02:28 +0800

    调整活动列表

--- src/main/java/com/dinglian/server/chuqulang/dao/impl/EventDaoImpl.java
+++ src/main/java/com/dinglian/server/chuqulang/dao/impl/EventDaoImpl.java
@@ -28,7 +28,7 @@ public class EventDaoImpl extends AbstractHibernateDao<Event> implements EventDa
 		/*if (searchCriteria.getCategory() != null) {
 			sb.append("left outer join fetch e.tags tags ");
 		}*/
-		sb.append("WHERE 1=1 ");
+		sb.append("WHERE 1=1 AND e.status != '0' AND e.startTime > :startTime ");
 		if (StringUtils.isNotBlank(searchCriteria.getKeyword())) {
 			sb.append("AND e.name like :keyword ");
 		}
@@ -36,40 +36,17 @@ public class EventDaoImpl extends AbstractHibernateDao<Event> implements EventDa
 		/*if (searchCriteria.getCategory() != null) {
 			sb.append("AND tags.tag.id = :category ");
 		}*/
-		/*if (StringUtils.isNotBlank(searchCriteria.getStatus())) {
-			if (searchCriteria.getStatus().equals(Event.STATUS_OVER)) {
-				sb.append("AND e.status = '" + Event.STATUS_OVER + "' ");
-			} else if (searchCriteria.getStatus().equals(Event.STATUS_PROGRESS)) {
-				sb.append("AND e.status = '" + Event.STATUS_PROGRESS + "' ");
-			} else if (searchCriteria.getStatus().equals(Event.STATUS_SIGNUP)) {
-				sb.append("AND e.status = '" + Event.STATUS_SIGNUP + "' ");
-			}
-		}*/
 		
 		// 开始时间升序排列
 		String orderBy = "ORDER BY e.startTime DESC ";
 		
-//		String orderBy = "ORDER BY e.creationDate DESC ";
-		/*if (StringUtils.isNotBlank(searchCriteria.getOrderBy())) {
-			if (Event.ORDERBY_DEFAULT.equals(searchCriteria.getOrderBy())) {
-				orderBy = "ORDER BY e.creationDate DESC,e.cost ";
-			} else if (Event.ORDERBY_CLOSEST.equals(searchCriteria.getOrderBy())) {
-				orderBy = "";
-			} else if (Event.ORDERBY_COST_ASC.equals(searchCriteria.getOrderBy())) {
-				orderBy = "ORDER BY e.cost ";
-			} else if (Event.ORDERBY_COST_DESC.equals(searchCriteria.getOrderBy())) {
-				orderBy = "ORDER BY e.cost DESC ";
-			}
-		}*/
 		sb.append(orderBy);
 		
 		Query query = getCurrentSession().createQuery(sb.toString());
+		query.setTimestamp("startTime", new Date());
 		if (StringUtils.isNotBlank(searchCriteria.getKeyword())) {
 			query.setString("keyword", "%" + searchCriteria.getKeyword() + "%");
 		}
-		/*if (searchCriteria.getCategory() != null) {
-			query.setInteger("category", searchCriteria.getCategory());
-		}*/
 		
 		if (searchCriteria.getPageSize() != 0) {
 			query.setFirstResult(searchCriteria.getStartRow());
